dd3c5c3e7609efc776b19d5d6bd96db57f4cb147
D1dii <didacgp123@gmail.com>
2025-01-23 13:44:42 +0100

    Player Controller

--- GameJamEvolution/Assets/Scripts/PlayerController.cs
+++ GameJamEvolution/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,124 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    [Header("Player Movement")]
+    private float movHorizontal = 0f;
+    [SerializeField] private float movSpeed;
+    [SerializeField] private float movSpeedRunning;
+    private float currentMovSpeed;
+    [Range(0, 0.5f)]
+    [SerializeField] private float smoothTime;
+    private Vector3 velocity = Vector3.zero;
+    private bool isLookingRight = true;
+
+    [Header("Player Jump")]
+    [SerializeField] private float jumpForce;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Vector3 boxSize;
+    [SerializeField] private bool isGrounded;
+    private bool jump = false;
+
+    [Header("Player Wall Jump")]
+    [SerializeField] private float wallJumpForce;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private Vector3 boxSizeWall;
+    [SerializeField] private bool isTouchingWall;
+    [SerializeField] private bool canWallJump = true;
+    private bool wallJump = false;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            currentMovSpeed = movSpeedRunning;
+        }
+        else
+        {
+            currentMovSpeed = movSpeed;
+        }
+
+        movHorizontal = Input.GetAxisRaw("Horizontal") * currentMovSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            wallJump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics.CheckBox(groundCheck.position, boxSize, Quaternion.identity, groundLayer);
+        isTouchingWall = Physics.CheckBox(wallCheck.position, boxSizeWall, Quaternion.identity, wallLayer);
+
+        if (isGrounded)
+        {
+            canWallJump = true;
+        }
+
+        MovePlayer(movHorizontal * Time.fixedDeltaTime, jump, wallJump);
+
+        jump = false;
+        wallJump = false;
+    }
+
+    private void MovePlayer(float move, bool jumping, bool wallJumping)
+    {
+        Vector3 targetVelocity = new Vector3(movHorizontal, rb.velocity.y, 0);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, smoothTime);
+
+        if (movHorizontal > 0 && !isLookingRight)
+        {
+            Flip();
+        }
+        else if (movHorizontal < 0 && isLookingRight)
+        {
+            Flip();
+        }
+
+        if (wallJumping && isTouchingWall && !isGrounded && canWallJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isTouchingWall = false;
+            canWallJump = false;
+        }
+
+        if (jumping && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+       
+
+    }
+
+    private void Flip()
+    {
+        isLookingRight = !isLookingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(groundCheck.position, boxSize);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(wallCheck.position, boxSizeWall);
     }
 }
