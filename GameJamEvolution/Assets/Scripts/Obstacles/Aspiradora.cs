d9f9fab72f7847ae194021b035f0174de1b9ee69
D1dii <didacgp123@gmail.com>
2025-01-23 18:51:35 +0100

    Aspiradora

---
+++ GameJamEvolution/Assets/Scripts/Obstacles/Aspiradora.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aspiradora : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 aspirationForce;
+    [SerializeField] private float aspirationTime;
+    [SerializeField] private float aspirationTimeCounter;
+    [SerializeField] private bool isAspiring = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!isAspiring)
+        {
+            aspirationTimeCounter += Time.deltaTime;
+        }
+        else if (isAspiring)
+        {
+            aspirationTimeCounter -= Time.deltaTime / 2;
+        }
+
+        if (aspirationTimeCounter >= aspirationTime)
+        {
+            isAspiring = true;
+        }
+        else if (aspirationTimeCounter <= 0)
+        {
+            isAspiring = false;
+        }
+
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && isAspiring)
+        {
+            other.gameObject.GetComponent<Rigidbody>().velocity -= aspirationForce;
+        }
+    }
+}
