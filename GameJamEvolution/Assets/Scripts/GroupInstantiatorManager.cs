eda750dce5a2577e666b42425fb3f0eaf7f8afdb
Samu163 <125267861+Samu163@users.noreply.github.com>
2025-01-26 10:52:30 +0100

    escena y bloques mas largos de plataformas

--- GameJamEvolution/Assets/Scripts/GroupInstantiatorManager.cs
+++ GameJamEvolution/Assets/Scripts/GroupInstantiatorManager.cs
@@ -10,7 +10,9 @@ public class GroupInstantiatorManager : MonoBehaviour
 
     public void InstantiateGroupObstacles(Obstacle obstaclePrefab, GridSystem gridSystem)
     {
-        int sizeX = Random.Range(2, 7);
+        int sizeX = 0;
+
+        sizeX = obstaclePrefab.cellType == GridSystem.CellType.Ground ? Random.Range(5, 10) : Random.Range(2, 7);
 
         Vector2Int size = new Vector2Int(sizeX, 1);
 
