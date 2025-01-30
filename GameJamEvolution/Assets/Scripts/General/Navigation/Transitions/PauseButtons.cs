0bf88d5fff5d2dbcc484a8a3ea9cc5eca35671dc
D1dii <didacgp123@gmail.com>
2025-01-30 10:28:57 +0100

    Update PauseButtons.cs

--- GameJamEvolution/Assets/Scripts/General/Navigation/Transitions/PauseButtons.cs
+++ GameJamEvolution/Assets/Scripts/General/Navigation/Transitions/PauseButtons.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class PauseButtons : MonoBehaviour
 {
     public Button resumeButton;
-    public Button settingsButton;
     public Button exitButton;
 
     public GameObject settingsCanvas;
@@ -15,8 +14,6 @@ public class PauseButtons : MonoBehaviour
     {
         resumeButton.onClick.AddListener(() => GameManager.Instance.ResumeGame());
         resumeButton.onClick.AddListener(() => gameObject.SetActive(false));
-        settingsButton.onClick.AddListener(() => settingsCanvas.SetActive(true));
-        settingsButton.onClick.AddListener(() => gameObject.SetActive(false));
         exitButton.onClick.AddListener(() => LevelManager.Instance.SaveProgress());
         exitButton.onClick.AddListener(() => GameManager.Instance.LoadSceneRequest("MainMenuScene"));
         exitButton.onClick.AddListener(() => GameManager.Instance.ResumeGame());
